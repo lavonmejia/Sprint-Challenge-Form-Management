fbe2623fd364818ce524038dfb5178f584916192
LaVon Mejia <lavonmejia@gmail.com>
2019-08-09 16:32:23 -0400

    tests corrected

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 import FormikSignUpForm from "./Form.js";
 import Registered from "./GetRequest";
+import { render } from "@testing-library/react";
 
 
 function App() {
- 
+   
  return (
     <div className="App">
       <FormikSignUpForm />
--- client/src/Form.test.js
+++ client/src/Form.test.js
@@ -1,32 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/react/cleanup-after-each";
-
-import FormikSignUpForm from "./Logic";
+import FormikSignUpForm from "./Form";
 
 describe("<FormikSignUpForm/>", () => {
   it("should render without crashing", () => {
     render(<FormikSignUpForm/>)
-  });
+  })
 
 
 it("should display 'terms of service, your name, password, and super'", () => {
-    const { getByText } = render(<FormikSignUpForm />)
+    const { getByText, getByPlaceholderText } = render(<FormikSignUpForm />)
 
     getByText(/terms of service/i)
-    getByText(/your name/i)
-    getByText(/password/i)
+    getByPlaceholderText(/your name/i)
+    getByPlaceholderText(/password/i)
     getByText(/super/i)
 });
-
+});
 
 describe('The Button Functionality' , () => {
+
      it ("should register that it has been clicked", () => {
        const component = render(<FormikSignUpForm />);
        const submitButton = component.getByTestId("submit");
-       fireEvent.mousedown(submitButton);
-   
-       }))
-       
-       ;
+       fireEvent.click(submitButton);
 
+})})
