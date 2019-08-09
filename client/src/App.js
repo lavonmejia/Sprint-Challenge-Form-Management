d4a4d048ef9d0c9f969fdc88d1d36df1f0cbe4da
LaVon Mejia <lavonmejia@gmail.com>
2019-08-09 11:53:01 -0400

    basic form with some styling

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FormikSignUpForm from './Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikSignUpForm />
+    </div>
+  );
+}
+
+export default App;
